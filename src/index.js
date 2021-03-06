5800bce74636228a08dea8d933994dae8a5a9847
DimaChekashov <foxdima99@gmail.com>
2021-03-06 20:49:12 +0300

    create server

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./app/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { addUser } from "./actions";
+import { setupSocket } from "./sockets/index";
 
 import chat from "./reducers";
 
 const store = createStore(chat);
+
+store.dispatch(addUser("Me"));
 
 ReactDOM.render(
     <Provider store={store}>
